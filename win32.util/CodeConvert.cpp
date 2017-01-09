be0d0d228039642697ced71cff97ec80f20452b4
Freddie Wu <freddie.wu@capitaledge.cn>
2017-01-09 16:01:09 +0800

    more elaborate error report on encoding conversion errors.

--- win32.util/CodeConvert.cpp
+++ win32.util/CodeConvert.cpp
@@ -7,24 +7,41 @@
 
 UINT const ml::CP_GB18030 = 54936;
 
-std::runtime_error getEncodeError(char const* prefix, DWORD const error) {
+std::ostringstream getEncodeErrorInfo(char const* prefix, DWORD const error) {
 	std::ostringstream buffer;
 	buffer << prefix << ':';
 	switch (error) {
 	case ERROR_INSUFFICIENT_BUFFER:
-		buffer << "ERROR_INSUFFICIENT_BUFFER";
+		buffer << "INSUFFICIENT_BUFFER";
 		break;
 	case ERROR_INVALID_FLAGS:
-		buffer << "ERROR_INVALID_FLAGS";
+		buffer << "INVALID_FLAGS";
 		break;
 	case ERROR_INVALID_PARAMETER:
-		buffer << "ERROR_INVALID_PARAMETER";
+		buffer << "INVALID_PARAMETER";
 		break;
 	case ERROR_NO_UNICODE_TRANSLATION:
-		buffer << "ERROR_NO_UNICODE_TRANSLATION";
+		buffer << "NO_UNICODE_TRANSLATION";
 		break;
 	default:
 		buffer << '[' << error << ']';
+	}
+	return buffer;
+}
+
+std::runtime_error getEncodeError(char const* prefix, DWORD const error, char const* str) {
+	std::ostringstream buffer = getEncodeErrorInfo(prefix, error);
+	if (NULL != str) buffer << " \"" << str << '"';
+	return std::runtime_error(buffer.str());
+}
+
+std::runtime_error getEncodeError(char const* prefix, DWORD const error, wchar_t const* str) {
+	std::ostringstream buffer = getEncodeErrorInfo(prefix, error);
+	if (NULL != str) {
+		size_t const len = std::wcslen(str) * sizeof(*str);
+		std::vector<char> strBuf(len + 1, '\0');
+		std::copy(reinterpret_cast<char const*>(str), reinterpret_cast<char const*>(str)+len, strBuf.begin());
+		buffer << " \"" << &strBuf[0] << '"';
 	}
 	return std::runtime_error(buffer.str());
 }
@@ -34,12 +51,12 @@ std::wstring ml::convert(UINT frCP, char const* inStr) throw(std::runtime_error)
 	if (std::strlen(inStr) == 0) return L"";
 	size_t const len = ::MultiByteToWideChar(frCP, MB_ERR_INVALID_CHARS, inStr, -1, NULL, 0);
 	if (len == 0) {
-		throw getEncodeError("MultiByteToWideChar<0>", ::GetLastError());
+		throw getEncodeError("MultiByteToWideChar<<", ::GetLastError(), inStr);
 	}
 	std::vector<wchar_t> outUcs(len, L'\0');
 	int const result = ::MultiByteToWideChar(frCP, MB_ERR_INVALID_CHARS, inStr, -1, &outUcs[0], len);
 	if (result == 0) {
-		throw getEncodeError("MultiByteToWideChar<1>", ::GetLastError());
+		throw getEncodeError("MultiByteToWideChar>>", ::GetLastError(), inStr);
 	}
 	assert(result == len);
 	assert(outUcs[len - 1] == L'\0');	//outUcs must be properly terminated!
@@ -52,12 +69,12 @@ std::string ml::convert(UINT toCP, wchar_t const* inUcs) throw(std::runtime_erro
 	DWORD const flags = (CP_UTF8 == toCP) ? WC_ERR_INVALID_CHARS : 0;
 	size_t const len = ::WideCharToMultiByte(toCP, flags, inUcs, -1, NULL, 0, NULL, NULL);
 	if (len == 0) {
-		throw getEncodeError("WideCharToMultiByte<0>", ::GetLastError());
+		throw getEncodeError("WideCharToMultiByte<<", ::GetLastError(), inUcs);
 	}
 	std::vector<char> outStr(len, '\0');
 	int const result = ::WideCharToMultiByte(toCP, flags, inUcs, -1, &outStr[0], len, NULL, NULL);
 	if (result == 0) {
-		throw getEncodeError("WideCharToMultiByte<1>", ::GetLastError());
+		throw getEncodeError("WideCharToMultiByte>>", ::GetLastError(), inUcs);
 	}
 	assert(result == len);
 	assert(outStr[len - 1] == '\0');	//outStr must be properly terminated!

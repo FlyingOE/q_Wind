db9afc761294329bf9bf06a52ca91c5e82e4f146
Freddie Wu <freddie.wu@capitaledge.cn>
2017-01-16 14:13:23 +0800

    TDF v3 API upgraded to 20161129

---
+++ inc/TDF_C++_v3/NonMDMsgDecoder.h
@@ -0,0 +1,63 @@
+#pragma pack(push)
+#pragma pack(1)
+
+#include <string.h>
+
+#ifndef UINT32
+#define UINT32 unsigned int
+#endif
+
+#if defined(WIN32) || defined(WIN64) || defined(_WINDOWS)
+#ifdef TDF_API_EXPORT
+#define NonMDMMsg __declspec(dllexport) 
+#else	
+#define NonMDMMsg __declspec(dllimport)
+#endif
+#else
+#define NonMDMMsg __attribute((visibility("default")))
+#endif
+
+struct _NonMD_Msg_Def;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	class MsgDecoder
+	{
+	public:
+		MsgDecoder()
+		{
+			pMsg = NULL;
+		}
+
+		NonMDMMsg int GetMsgType();
+
+		NonMDMMsg bool Init(char* _pMsg, int size);
+
+		NonMDMMsg int GetInt32Field(int index);
+
+		NonMDMMsg __int64 GetInt64Field(int index);
+
+		NonMDMMsg unsigned __int64 GetUInt64Field(int index);
+
+		NonMDMMsg UINT32 GetUInt32Field(int index);
+
+		NonMDMMsg float GetFloatField(int index);
+
+		NonMDMMsg double GetDoubleField(int index);
+
+		NonMDMMsg char GetCharField(int index);
+
+		NonMDMMsg char* GetStringField(int index);
+
+	private:
+		_NonMD_Msg_Def* pDef;
+		char* pMsg;
+	};
+
+#ifdef __cplusplus
+}
+#endif
+
+#pragma pack(pop)
--- inc/TDF_C++_v3/TDFAPI.h
+++ inc/TDF_C++_v3/TDFAPI.h
@@ -2,7 +2,6 @@
 #define __TDF_API_H__
 
 #include "TDFAPIStruct.h"
-#include "TDFAPIVersion.h"
 
 #if defined(WIN32) || defined(WIN64) || defined(_WINDOWS)
 #ifdef TDF_API_EXPORT
@@ -11,7 +10,7 @@
 #define TDFAPI __declspec(dllimport)
 #endif
 #else
-#define TDFAPI
+#define TDFAPI __attribute((visibility("default")))
 #endif
 
 #ifdef __cplusplus
@@ -40,7 +39,7 @@ TDFAPI int TDF_SetEnv(TDF_ENVIRON_SETTING nEnv, unsigned int nValue);
 
 //同步函数，打开到TDFServer的连接，如果成功，则返回句柄，否则返回NULL，在TDF_Open期间发生了网络断开，将不会自动重连
 //在调用期间，系统通知函数将收到MSG_SYS_CONNECT_RESULT，MSG_SYS_LOGIN_RESULT，MSG_SYS_CODETABLE_RESULT消息
-//如果网络断开，则会收到MSG_SYS_DISCONNECT_NETWORK，pErr中存放错误代码，只有在错误代码为 TDF_ERR_NETWORK_ERROR 时候，外部才应该做重连逻辑
+//如果网络断开，则会收到MSG_SYS_DISCONNECT_NETWORK，pErr中存放错误代码
 TDFAPI THANDLE TDF_Open(TDF_OPEN_SETTING* pSettings, TDF_ERR* pErr);
 //可以配置多组服务器，取多组服务器中最快的行情推送
 TDFAPI THANDLE TDF_OpenExt(TDF_OPEN_SETTING_EXT* pSettings,TDF_ERR* pErr);
@@ -48,7 +47,7 @@ TDFAPI THANDLE TDF_OpenExt(TDF_OPEN_SETTING_EXT* pSettings,TDF_ERR* pErr);
 TDFAPI THANDLE TDF_OpenProxy(TDF_OPEN_SETTING* pOpenSettings, TDF_PROXY_SETTING* pProxySettings, TDF_ERR* pErr);
 TDFAPI THANDLE TDF_OpenProxyExt(TDF_OPEN_SETTING_EXT* pOpenSettings, TDF_PROXY_SETTING* pProxySettings, TDF_ERR* pErr);
 
-//获取指定市场的代码表，如果为空，则获取所有的代码表，在已经收到MSG_SYS_CODETABLE_RESULT 消息之后，可以获得代码表
+//获取指定市场的代码表，在已经收到MSG_SYS_CODETABLE_RESULT 消息之后，可以获得代码表
 //获取到的代码表，需要调用TDF_FreeArr来释放内存
 TDFAPI int TDF_GetCodeTable(THANDLE hTdf, const char* szMarket, TDF_CODE** pCode, unsigned int* pItems);
 
@@ -58,13 +57,34 @@ TDFAPI int TDF_GetCodeTable(THANDLE hTdf, const char* szMarket, TDF_CODE** pCode
 
 TDFAPI int TDF_GetOptionCodeInfo(THANDLE hTdf, const char* szWindCode, TDF_OPTION_CODE* pCodeInfo);
 
-//同步函数，关闭连接，不要再回调函数里面调用，否则会卡死
+//同步函数，关闭连接，不要在回调函数里面调用，否则会卡死
 TDFAPI int TDF_Close(THANDLE hTdf);
 
 TDFAPI void TDF_FreeArr(void *pArr);
 
 //登陆后订阅; 此函数是个异步函数，最好在TDF_Open成功之后调用
 TDFAPI int TDF_SetSubscription(THANDLE hTdf, const char* szSubScriptions, SUBSCRIPTION_STYLE nSubStyle);
+
+//打开获取参考数据的服务
+TDFAPI THANDLE TDF_Open_RefData(TDF_OPEN_REFDATA_SETTING* pSettings, TDF_ERR* pErr);
+
+//获取ETF清单
+TDFAPI int TDF_ReqETFList(THANDLE hTdf, int reqID, const char* szMarketAbbr, const char* reqCode, int reqDate, int dataLevel, int marektSource = 0);
+
+//获取连接状态信息
+TDFAPI int TDF_GetConStatInfo(THANDLE hTdf, int conIndex, ConStatInfo* pConsStatInfo);
+
+//释放全局的内存：整个程序退出时，如果需要测试内存分配，可调用释放全局内存. 调用后，API库不可用
+TDFAPI void TDF_EXIT();
+//
+TDFAPI const char* TDF_Version();
+
+//获取当前最新行情,需调用TDF_FreeArr(void*)释放内存
+TDFAPI int TDF_GetLastMarketData(THANDLE hTdf, const char* szMarket, TDF_MARKET_DATA** pMarketData, int* nItems);
+//获取当前最新指数,需调用TDF_FreeArr(void*)释放内存
+TDFAPI int TDF_GetLastIndexData(THANDLE hTdf, const char* szMarket, TDF_INDEX_DATA** pIndexData, int* nItems);
+//获取当前最新期货(期权),需调用TDF_FreeArr(void*)释放内存
+TDFAPI int TDF_GetLastFutureData(THANDLE hTdf, const char* szMarket, TDF_FUTURE_DATA** pFutureData, int* nItems);
 
 #ifdef __cplusplus
 }
